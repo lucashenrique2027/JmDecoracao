c0b6fc07b5b054f3818216d9dca1496304e0a1b5
Lucas <lh0487552@gmail.com>
2025-09-18 22:37:14 -0300

    arquivos sem uso

--- models/User.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const userSchema = new mongoose.Schema({
-  nome: String,
-  email: { type: String, unique: true },
-  senha: String,
-});
-
-export default mongoose.model('User', userSchema);
